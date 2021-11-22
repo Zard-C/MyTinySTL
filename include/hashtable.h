94a0fb164deb1192e06c2ea1cc8b85985a6db864
zhangtonghe <zhangtonghe@china-aicc.cn>
2021-11-22 01:41:19 +0000

    hashtable.h and dependency

---
+++ include/hashtable.h
@@ -0,0 +1,31 @@
+#ifndef MYSTL_HASHTABLE_H_ 
+#define MYSTL_HASHTABLE_H_ 
+
+
+// template hashtable 
+// 使用开链法来解决哈希冲突
+
+#include <initializer_list> . 
+
+#include "algo.h"
+#include "functional.h"
+#include "memory.h"
+#include "vector.h"
+#include "util.h"
+#include "exceptdef.h"
+
+
+
+namespace mystl
+{
+
+
+
+
+
+
+
+
+}   // end of namespace mystl 
+#endif 
+
